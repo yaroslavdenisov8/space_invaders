07b10ccadcace22bb74859fb980430d5ae43f5c9
Iskander <iskander.akhtaryanov@bk.ru>
2022-12-26 18:32:46 +0500

    Изменен текст главного экрана, смена экрана теперь происходит через shift

--- start_screen.py
+++ start_screen.py
@@ -12,18 +12,18 @@ def terminate():
 
 
 def start_screen():
-    intro_text = ["ЗАСТАВКА"]
+    intro_text = ['---Press SHIFT to start---']
     pygame.init()
     fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
-    text_coord = 100
+    text_coord = 345
     for line in intro_text:
         string_rendered = font.render(line, 1, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 250
+        intro_rect.x = 175
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
 
@@ -31,7 +31,7 @@ def start_screen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SHIFT:
+                    return
         pygame.display.flip()
