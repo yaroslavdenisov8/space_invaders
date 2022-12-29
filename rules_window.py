2ffc22f62f8f7f6aa35a96f97c07a7ac04318db5
Iskander <iskander.akhtaryanov@bk.ru>
2022-12-29 16:59:13 +0500

    неюольшие изменения

--- rules_window.py
+++ rules_window.py
@@ -1,9 +1,8 @@
 import pygame
 import sys
 from Load_images import load_image
-from rules_window import open_rules_window
 from button import Button
-from main import main
+from start_screen import start_screen
 
 size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
@@ -14,24 +13,22 @@ def terminate():
     sys.exit()
 
 
-def start_screen():
-    intro_text = []
-    pygame.display.set_caption('Меню')
+def open_rules_window():
+    intro_text = ["Правила"]
+    pygame.display.set_caption('Правила')
     pygame.init()
     fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
-    text_coord = 345
+    text_coord = 30
     button = Button(90, 50)
-    button.draw(175, 345, 'Играть', screen)
-    rules_button = Button(90, 50)
-    rules_button.draw(286, 345, 'Правила', screen)
+    button.draw(10, 10, 'Назад', screen)
     for line in intro_text:
         string_rendered = font.render(line, 1, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 175
+        intro_rect.x = 100
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
 
@@ -40,7 +37,5 @@ def start_screen():
             if event.type == pygame.QUIT:
                 terminate()
             if button.get_pressed():
-                main()
-            if rules_button.get_pressed():
-                open_rules_window()
+                start_screen()
         pygame.display.flip()
