bf62087c363a09347b10987923b73f6795f65fde
Iskander <iskander.akhtaryanov@bk.ru>
2022-12-29 18:44:01 +0500

    Сменил цвет кнопок

--- button.py
+++ button.py
@@ -10,11 +10,11 @@ class Button:
     def draw(self, x, y, message, screen):
         self.x = x
         self.y = y
-        pygame.draw.rect(screen, 'white', (x, y, self.width, self.height))
+        pygame.draw.rect(screen, (255, 229, 180), (x, y, self.width, self.height))
         font = pygame.font.Font(None, 30)
-        text = font.render(f'{message}', True, 'black')
-        text_x = x + 10
-        text_y = y + 15
+        text = font.render(f'{message}', True, (220, 20, 60))
+        text_x = self.width // 2 - text.get_width() // 2 + self.x
+        text_y = self.height // 2 - text.get_height() // 2 + self.y
         screen.blit(text, (text_x, text_y, self.width - 10, self.height - 10))
 
     def get_pressed(self):
