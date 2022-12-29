3df117c9bc98b859ba34157fcb2f0c56c364e3f0
Iskander <iskander.akhtaryanov@bk.ru>
2022-12-29 17:57:06 +0500

    экран заставки

--- Start_screen.py
+++ Start_screen.py
@@ -25,21 +25,21 @@ def start_screen():
     button.draw(175, 345, 'Играть', screen)
     rules_button = Button(90, 50)
     rules_button.draw(286, 345, 'Правила', screen)
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 175
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+    # for line in intro_text:
+    #     string_rendered = font.render(line, 1, pygame.Color('white'))
+    #     intro_rect = string_rendered.get_rect()
+    #     text_coord += 10
+    #     intro_rect.top = text_coord
+    #     intro_rect.x = 175
+    #     text_coord += intro_rect.height
+    #     screen.blit(string_rendered, intro_rect)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            if button.get_pressed():
-                return
             if rules_button.get_pressed():
                 rules_window.open_rules_window()
+            if button.get_pressed():
+                return
         pygame.display.flip()
